12a2ad6639d24ac5082082f8fe909f571b371ad3
Ian Zunderdorp <mail@kutagh.com>
2013-11-05 15:57:28 +0100

    PaymentYetToReceive/PaymentYetToMake both working :)

--- Handler/User.hs
+++ Handler/User.hs
@@ -3,7 +3,8 @@ module Handler.User where
 
 import Import
 import Data.Maybe
-import Data.Map hiding (null, map)
+import Data.Map hiding (null, map, foldr)
+import Data.Set hiding (null, map, foldr, member, empty)
 import Text.Shakespeare.Text
 
 getAllUsersR :: Handler Html
@@ -31,6 +32,8 @@ getUserR id = do
             debtsOfUser <- getDebtsByUser id
             paidByUser <- getPaymentsMadeByUser id
             sentByUser <- getPaymentsReceivedByUser id
+            yetToReceive <- paymentsYetToReceive id
+            yetToMake <- paymentsYetToMake id
             rbuHtml <- printAllReceipts receiptsByUser (userIdent user)
             douHtml <- printAllDebts debtsOfUser (userIdent user)
             pbuHtml <- printAllPayments 
@@ -47,7 +50,19 @@ getUserR id = do
                 ("received" :: Text) 
                 ("from" :: Text) 
                 sentByUser
-            composed <- composer [rbuHtml, douHtml, pbuHtml, sbuHtml]
+            ytrHtml <- printYet
+                ("Payments yet to receive" :: Text)
+                ("No payments yet to be received" :: Text)
+                ("Expecting" :: Text)
+                ("from" :: Text)
+                (Data.Map.toList yetToReceive)
+            ytmHtml <- printYet
+                ("Payments yet to make" :: Text)
+                ("No payments to be made" :: Text)
+                ("Yet to pay" :: Text)
+                ("to" :: Text)
+                (Data.Map.toList yetToMake)
+            composed <- composer [rbuHtml, douHtml, pbuHtml, sbuHtml, ytrHtml, ytmHtml]
             renderer <- getUrlRenderParams
             return (composed renderer)
         Nothing -> return [shamlet|User not found|]
@@ -82,26 +97,87 @@ printAllPayments header empty user action direction payments = return [hamlet|
         $forall (Entity pid (Payment time from to amount), Entity tid tu) <- payments
             <p>Payment #{show pid} made at #{show time}, #{action} #{show amount} #{direction} <a href=@{UserR tid}>#{userIdent tu}</a>.
     |]
+
+printYet header empty keyword direction datamap = return [hamlet|
+    $if null datamap
+        <p>#{empty}
+    $else
+        <p>#{header}
+        $forall (id, amount) <- datamap
+            <li>#{keyword} #{amount} #{direction} <a href=@{UserR id}>#{show id}</a>
+    |]
     
 paymentsYetToReceive userId = do
     receipts <- runDB $ selectList [ReceiptPaidBy ==. userId] []
     joined <- joiner receipts
-    converted <- test joined empty
-    return receipts
+    let 
+        debtPerUser = test joined empty
+        users = Data.Set.toList (keysSet debtPerUser)
+    paymentsPerUser <- getPayments userId users (return empty)
+    let filtered = Data.Map.filter (>0) $ unionWith (-) debtPerUser $ filterWithKey (\k _ -> member k debtPerUser) paymentsPerUser
+    return filtered
     where
         joiner ((Entity rid r):rs) = do
-            debts <- selectList [ReceiptUserReceipt ==. rid] []
+            debts <- runDB $ selectList [ReceiptUserReceipt ==. rid] []
             tail <- joiner rs
             return ((Entity rid r, debts):tail)
-        test ((Entity rid r, users):xs) datamap = updater users (test xs datamap) where
+        joiner [] = return []
+        test ((Entity rid r, users):xs) datamap = test xs (updater users datamap) where
             updater ((Entity ruid ru):rus) dm
-                | member (receiptUserUser ru) dm = adjust (+avgDebt) (receiptUserUser ru) dm
-                | otherwise = Data.Map.insert (receiptUserUser ru) avgDebt dm
-            updater [] datamap = datamap
+                | member (receiptUserUser ru) dm = adjust (+avgDebt) uid (updater rus dm)
+                | otherwise = Data.Map.insert uid avgDebt (updater rus dm)
+                where uid = receiptUserUser ru
+            updater [] dm = dm
             avgDebt = div (receiptPaidTotal r) (length users)
         test [] datamap = datamap
+        getPayments id (userId:us) datamap = do
+            tail <- getPayments id us datamap
+            payments <- runDB $ selectList [PaymentTo ==. id, PaymentFrom ==. userId] []
+            let result = updater payments tail
+            return result
+            where
+                updater ((Entity pid p):ps) dm
+                    | member userId dm = updater ps (adjust (+(paymentAmount p)) userId dm)
+                    | otherwise = updater ps (Data.Map.insert userId (paymentAmount p) dm)
+                updater [] dm = dm
+        getPayments id [] dm = dm
 
-    
+
+paymentsYetToMake id = do
+    receiptUsers <- runDB $ selectList [ReceiptUserUser ==. id] []
+    receipts <- joiner receiptUsers
+    let 
+        debtPerUser = test receipts empty
+        users = Data.Set.toList (keysSet debtPerUser)
+    paymentsPerUser <- getPayments id users (return empty)
+    let filtered = Data.Map.filter (>0) $ unionWith (-) debtPerUser $ filterWithKey (\k _ -> member k debtPerUser) paymentsPerUser
+    return filtered
+    where
+        joiner ((Entity ruid ru):rus) = do
+            receipt <- runDB $ getJust rid
+            debtors <- runDB $ selectList [ReceiptUserReceipt ==. rid] []
+            tail <- joiner rus
+            return ((receipt, (div (receiptPaidTotal receipt) (length debtors))):tail)
+            where
+                rid = receiptUserReceipt ru
+        joiner [] = return []
+        test (x:xs) datamap = test xs (updater x datamap) where
+            debtTo = receiptPaidBy $ fst x
+            updater (receipt, debt) dm
+                | member debtTo dm = adjust (+debt) debtTo dm
+                | otherwise = Data.Map.insert debtTo debt dm
+        test [] datamap = datamap
+        getPayments id (userId:us) datamap = do
+            tail <- getPayments id us datamap
+            payments <- runDB $ selectList [PaymentTo ==. userId, PaymentFrom ==. id] []
+            let result = updater payments tail
+            return result
+            where
+                updater ((Entity pid p):ps) dm
+                    | member userId dm = updater ps (adjust (+(paymentAmount p)) userId dm)
+                    | otherwise = updater ps (Data.Map.insert userId (paymentAmount p) dm)
+                updater [] dm = dm
+        getPayments _ [] datamap = datamap
 composer toCompose = return [hamlet|
     $if null toCompose
         <p>Error: Tried to compose zero items
